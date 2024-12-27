ff1f77882b2d9d07ac3015b48d532b1e161076c7
kaceykooGithub <kacey.koo@gmail.com>
2024-12-27 11:43:54 +0000

    categories

---
+++ pages.js
@@ -0,0 +1,62 @@
+const all = [
+     "intro",
+     "social"
+];
+shown = "";
+
+function changeText(string) {
+     var lang = $('#lang-switch').val();
+     if (lang === 'jp') {
+          var request = new XMLHttpRequest();
+          request.open("GET", "../lang/ja.json", false);
+          request.send(null)
+          var json = JSON.parse(request.responseText);
+     } else if (lang === 'en') {
+          var request = new XMLHttpRequest();
+          request.open("GET", "../lang/en.json", false);
+          request.send(null)
+          var json = JSON.parse(request.responseText);
+     }
+     for (key in json) {
+          if (string === "") {
+               document.getElementById(`location`).innerHTML = json.category_home;
+               return;
+          }
+          if (!all.includes(shown)) {
+               document.getElementById(`location`).innerHTML = json.category_home;
+               return;
+          }
+          if (key === `category_${string}`) {
+               document.getElementById(`location`).innerHTML = json[key];
+               return;
+          }
+     }
+}
+function changeHold(string) {
+     if (shown !== string) {
+          for (const key of all) {
+               document.getElementById(`category_${key}-div`).classList.add("is-hidden");
+               document.getElementById(`category_${key}`).classList.remove("is-active");
+          }
+
+          document.getElementById(`category_${string}-div`).classList.remove("is-hidden");
+          document.getElementById(`category_${string}`).classList.add("is-active");
+
+          shown = string;
+     } else {
+          for (const key of all) {
+               document.getElementById(`category_${key}-div`).classList.add("is-hidden");
+               document.getElementById(`category_${key}`).classList.remove("is-active");
+          }
+          shown = "";
+     }
+     changeText(string);
+}
+
+for (const key of all) {
+     document.getElementById(`category_${key}-div`).classList.add("is-hidden");
+     changeText(`${key}`);
+     document.getElementById(`category_${key}`).addEventListener('click', () => {
+          changeHold(`${key}`);
+     });
+};
